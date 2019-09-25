f6222af4112954a64971d7b5d213ec5bf6622f0e
Maxime Deboffle <maxime.deboffle@hotmail.com>
2019-09-25 10:12:24 +0200

    Listening on port 8080 and sends HelloWorld when a get request is received

---
+++ README.md
@@ -0,0 +1,3 @@
+Chatbot nodeJS avec express
+
+renvoie un hello world lorsqu'on accède au port 8080 via une requête get
--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+express = require("express");
+app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+  })
+app.listen(8080, () => console.log("Listening on port http://localhost:8080"));
